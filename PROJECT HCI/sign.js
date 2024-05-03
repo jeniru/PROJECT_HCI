9dbf2fbaa2a2620d5857618a290e37a33b895dc4
adylhide6789 <roseadelhiede.ahat@hcdc.edu.ph>
2024-05-03 13:40:28 +0800

    hubhu

--- PROJECT HCI/sign.js
+++ PROJECT HCI/sign.js
@@ -1,13 +1,17 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('btnLogin-popup');
-const iconClose = document.querySelector('icon-close');
+const btnPopup = document.querySelector('.btnLogin-popup'); // Corrected selector
+const iconClose = document.querySelector('.icon-close'); // Corrected selector
 
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
 });
 
-loginLink.addEventListener('click', ()=> {
+loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
+});
+
+iconClose.addEventListener('click', () => {
+    window.location.href = 'index.html'; // Redirect to the home page
 });
