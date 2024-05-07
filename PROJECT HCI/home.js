7acb6e1d2dd5bbeeaf74594f6b5c0b9fdd7436d8
adylhide6789 <roseadelhiede.ahat@hcdc.edu.ph>
2024-05-08 06:52:05 +0800

    adi loves u

--- PROJECT HCI/home.js
+++ PROJECT HCI/home.js
@@ -7,3 +7,5 @@ search.addEventListener('focus',(event)=>{
 search.addEventListener('focusout',(event)=>{
     document.getElementById('search-wrapper').style.border="1px solid rgba(0, 0, 0, 0.276)";
 });
+
+
