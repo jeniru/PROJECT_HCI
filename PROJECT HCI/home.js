15e0cad7edd0fe4d4f9575957419b7ddc64c9880
adylhide6789 <roseadelhiede.ahat@hcdc.edu.ph>
2024-05-07 11:17:12 +0800

    HAHAHAHAHAHA

---
+++ PROJECT HCI/home.js
@@ -0,0 +1,9 @@
+var search=document.getElementById('search');
+
+search.addEventListener('focus',(event)=>{
+  document.getElementById('search-wrapper').style.border="1px solid #1dbf73";
+});
+
+search.addEventListener('focusout',(event)=>{
+    document.getElementById('search-wrapper').style.border="1px solid rgba(0, 0, 0, 0.276)";
+});
